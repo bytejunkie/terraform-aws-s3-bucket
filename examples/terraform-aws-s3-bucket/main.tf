8f9e34fdd27315b4e90cd3b3d67fd1987168f5ef
Matt Short <matt.short@coveainsurance.co.uk>
2022-06-23 16:54:47 +0100

    moving provider to module Version: 1.0.4

--- examples/terraform-aws-s3-bucket/main.tf
+++ examples/terraform-aws-s3-bucket/main.tf
@@ -2,9 +2,8 @@
 module "s3-buckets" {
   for_each = toset(local.bucket_names)
 
-  # source  = "bytejunkie/s3-bucket/aws"
-  # version = "1.0.0"
-  source = "../.."
+  source  = "bytejunkie/s3-bucket/aws//examples/terraform-aws-s3-bucket"
+  version = "1.0.3"
 
   bucket_name = each.value
 
