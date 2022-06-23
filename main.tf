700cf7b40dd058c775eb3eb17d2df08b77b1c743
Matt Short <matt.short@coveainsurance.co.uk>
2022-06-23 16:11:43 +0100

    - add code to handle multiple requests

--- main.tf
+++ main.tf
@@ -2,6 +2,8 @@ resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
   acl    = "private"
 
+  force_destroy = var.force_destroy
+  
   tags = {
     Name      = var.bucket_name
     Module_By = "Bytejunkie77"
--- variables.tf
+++ variables.tf
@@ -2,3 +2,9 @@ variable "bucket_name" {
   type        = string
   description = "(Required) The name for the bucket"
 }
+
+
+variable "force_destroy" {
+  type        = string
+  description = "(Required) The force_destroy setting for the bucket"
+}
