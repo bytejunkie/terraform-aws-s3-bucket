07079e5fb24636a8e6010417392ed073f18629a2
Matt Short <matt.short@coveainsurance.co.uk>
2022-06-26 09:03:04 +0100

    adding some tests,

--- examples/terraform-aws-s3-bucket/main.tf
+++ examples/terraform-aws-s3-bucket/main.tf
@@ -2,15 +2,15 @@
 module "s3-buckets" {
   for_each = toset(local.bucket_names)
 
-  source = "../.."
-  # source  = "bytejunkie/s3-bucket/aws"
-  # version = "1.0.4"
+  # source = "../.."
+  source  = "bytejunkie/s3-bucket/aws"
+  version = "1.0.5"
 
   bucket_name = each.value
 
   force_destroy            = local.local_data.buckets[each.value].force_destroy
   bucket_versioning_status = local.local_data.buckets[each.value].bucket_versioning_status
 
-  expiration_rules = local.local_data.buckets[each.value].lifecycle_rules.expiration
+  # expiration_rules = local.local_data.buckets[each.value].lifecycle_rules.expiration
 
 }
